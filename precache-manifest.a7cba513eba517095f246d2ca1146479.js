b5d9f3d4581a36c5087d6b121aee92b0591215eb
sunnySun-gituser <57857509+sunnySun-gituser@users.noreply.github.com>
2020-01-17 15:37:22 -0800

    Updates

---
+++ precache-manifest.a7cba513eba517095f246d2ca1146479.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90201fb8a2089697bb72b792e12aaa30",
+    "url": "/sunnys-moonshot/index.html"
+  },
+  {
+    "revision": "3a0348c0b21cb6f90eaa",
+    "url": "/sunnys-moonshot/static/js/2.135171e8.chunk.js"
+  },
+  {
+    "revision": "03fd7a11817e5e5db2f9",
+    "url": "/sunnys-moonshot/static/js/main.889ecd69.chunk.js"
+  },
+  {
+    "revision": "b17e72c10d5e4b7af444",
+    "url": "/sunnys-moonshot/static/js/runtime~main.0a993f48.js"
+  },
+  {
+    "revision": "07d5ba4a894c77d6f799ecde7e84850f",
+    "url": "/sunnys-moonshot/static/media/cart.07d5ba4a.svg"
+  },
+  {
+    "revision": "a8a3ba3a10003218c2a017cf68d20489",
+    "url": "/sunnys-moonshot/static/media/curve.a8a3ba3a.svg"
+  },
+  {
+    "revision": "f25b4cba48a496093085f268698a4b39",
+    "url": "/sunnys-moonshot/static/media/dog-1.f25b4cba.png"
+  },
+  {
+    "revision": "ab7a533bb445ea0d167a34cdc0eec387",
+    "url": "/sunnys-moonshot/static/media/dog-2.ab7a533b.png"
+  },
+  {
+    "revision": "b62803749fd80890a425b0dfae080c47",
+    "url": "/sunnys-moonshot/static/media/dog-3.b6280374.png"
+  },
+  {
+    "revision": "98f2a8b1296f385203e7b9b5d9938fcf",
+    "url": "/sunnys-moonshot/static/media/exit.98f2a8b1.svg"
+  },
+  {
+    "revision": "0b1c67758d564d5eb140740847e6886e",
+    "url": "/sunnys-moonshot/static/media/galaxy.0b1c6775.jpg"
+  },
+  {
+    "revision": "3192f86cb3a13b08200f5e3d3fef0325",
+    "url": "/sunnys-moonshot/static/media/home.3192f86c.svg"
+  },
+  {
+    "revision": "48932214f494cf346794f464ee2c04e3",
+    "url": "/sunnys-moonshot/static/media/iss.48932214.jpg"
+  },
+  {
+    "revision": "0c7bf061e88c942e5031e971c6ab447b",
+    "url": "/sunnys-moonshot/static/media/logo.0c7bf061.svg"
+  },
+  {
+    "revision": "493bbb752333aacb0e80bab31c2e925a",
+    "url": "/sunnys-moonshot/static/media/moon.493bbb75.jpg"
+  },
+  {
+    "revision": "ea589b574fff44db577387da01243704",
+    "url": "/sunnys-moonshot/static/media/profile.ea589b57.svg"
+  },
+  {
+    "revision": "c27cc1f53fed31cbbc1d743f9bbaba1d",
+    "url": "/sunnys-moonshot/static/media/rocket.c27cc1f5.svg"
+  },
+  {
+    "revision": "7dfc79232cbb090253471fe744fe5680",
+    "url": "/sunnys-moonshot/static/media/space.7dfc7923.jpg"
+  }
+]);
